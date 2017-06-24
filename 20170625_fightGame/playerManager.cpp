a37bc2332648e9c47c8f2e8b26a7bb1633e844b8
kijun <kian3149@gmail.com>
2017-06-25 01:06:20 +0900

    터지지않길....

--- 20170625_fightGame/playerManager.cpp
+++ 20170625_fightGame/playerManager.cpp
@@ -16,13 +16,25 @@ HRESULT playerManager::init()
 
 	//상속받고 init에 이런식으로 초기화 해주기
 
-	_player.img = IMAGEMANAGER->addFrameImage("이동", "move.bmp", 0, 0, 288, 44, 6, 1, true, RGB(255, 0, 255));
+	_player.img = IMAGEMANAGER->addFrameImage("사스케", "Sasuke.bmp", 0, 0, 1090, 728, 10, 14, true, RGB(255, 0, 255));
 
-	int rightMove[] = { 0,1,2,3,4,5 };
-	KEYANIMANAGER->addArrayFrameAnimation("오른쪽이동", "이동", rightMove, 6, 10, true);
+	int rightStop[] = { 0,1,2,3 };
+	KEYANIMANAGER->addArrayFrameAnimation("오른쪽정지", "사스케", rightStop, 4, 10, true);
 
-	int rightStop[] = { 0 };
-	KEYANIMANAGER->addArrayFrameAnimation("오른쪽정지", "이동", rightStop, 1, 10, true);
+	int rightMove[] = { 4,5,6,7,8,9 };
+	KEYANIMANAGER->addArrayFrameAnimation("오른쪽이동", "사스케", rightMove, 6, 10, true);
+
+	int leftStop[] = { 10,11,12,13 };
+	KEYANIMANAGER->addArrayFrameAnimation("왼쪽정지", "사스케", leftStop, 4, 10, true);
+
+	int leftMove[] = { 14,15,16,17,18,19 };
+	KEYANIMANAGER->addArrayFrameAnimation("왼쪽이동", "사스케", leftMove, 6, 10, true);
+
+	int rightRunning[] = { 24,25,26,27,28,29 };
+	KEYANIMANAGER->addArrayFrameAnimation("오른쪽달리기", "사스케", rightRunning, 6, 10, true);
+
+	int leftRunning[] = { 34,35,36,37,38,39 };
+	KEYANIMANAGER->addArrayFrameAnimation("왼쪽달리기", "사스케", leftRunning, 4, 10, true);
 
 	_player.currentHp = _player.maxHp = 100.0f;
 	_player.gravity = 5.0f;
@@ -101,10 +113,14 @@ void playerManager::inputKey()
 	if (KEYMANAGER->isOnceKeyDown(VK_LEFT))
 	{
 		_player.playerState = PLAYERSTATE_LEFT_MOVE;
+		_player.ani = KEYANIMANAGER->findAnimation("왼쪽이동");
+		_player.ani->start();
 	}
 	else if (KEYMANAGER->isOnceKeyUp(VK_LEFT))
 	{
 		_player.playerState = PLAYERSTATE_LEFT_STOP;
+		_player.ani = KEYANIMANAGER->findAnimation("왼쪽정지");
+		_player.ani->start();
 	}
 
 	if (KEYMANAGER->isOnceKeyDown(VK_SPACE))
